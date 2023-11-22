52417ace6ff0523f1119873d82604750ad59ba21
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-11-22 18:24:22 -0300

    feat: add validation

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { paymentFactory } from "./factories/factories";
+import { ValidationMiddlewares } from "./middlewares/validation-middlewares";
 
 const paymentControllerFactory = paymentFactory();
 const routes = Router();
 routes.get("/", (req, res) => res.send("I'm working"));
-routes.post("/checkout-payment", paymentControllerFactory.makeCheckout);
+routes.post(
+  "/checkout-payment",
+  ValidationMiddlewares.checkoutPayment,
+  paymentControllerFactory.makeCheckout
+);
 routes.put("/order-id", paymentControllerFactory.switchOrderStatus);
 
 export { routes };
